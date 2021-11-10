655b6253d8fa838387da100b6912c3cabae64e15
Tinul Piumika <it20188054@my.sliit.lk>
2021-11-11 01:35:52 +0530

    deployment build

---
+++ build/precache-manifest.ebb8d4d0ed8b967966e7fff5630f537c.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af0b260f92b02efbb40f09d173d8cf8",
+    "url": "/batp/index.html"
+  },
+  {
+    "revision": "55a3405fb4049fd4c521",
+    "url": "/batp/static/css/2.e62a8ce7.chunk.css"
+  },
+  {
+    "revision": "e3995faa078e326abee4",
+    "url": "/batp/static/css/main.072332b3.chunk.css"
+  },
+  {
+    "revision": "55a3405fb4049fd4c521",
+    "url": "/batp/static/js/2.c50c8bfb.chunk.js"
+  },
+  {
+    "revision": "332bf1b0d236b7fc5455596e6356764c",
+    "url": "/batp/static/js/2.c50c8bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3995faa078e326abee4",
+    "url": "/batp/static/js/main.b2805a40.chunk.js"
+  },
+  {
+    "revision": "1cf3c58cf5d75a00412b",
+    "url": "/batp/static/js/runtime-main.6462f610.js"
+  },
+  {
+    "revision": "993364ca2f757e4e57151ff7f8a8be03",
+    "url": "/batp/static/media/VIMS1.993364ca.png"
+  },
+  {
+    "revision": "8f0b4fe9198a1093deba1f94965362e3",
+    "url": "/batp/static/media/VIMS2.8f0b4fe9.png"
+  },
+  {
+    "revision": "82e0289115107d21596d1234a375fb66",
+    "url": "/batp/static/media/VIMS3.82e02891.png"
+  },
+  {
+    "revision": "39d3f53fff6aacb737f23db075be4d90",
+    "url": "/batp/static/media/VIMS4.39d3f53f.png"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/batp/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/batp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/batp/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/batp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/batp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/batp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e8ebd95c33575cece71acd9b358baa2e",
+    "url": "/batp/static/media/gq1.e8ebd95c.png"
+  },
+  {
+    "revision": "eaa2e7f7ffde70a64cb67955c538c836",
+    "url": "/batp/static/media/gq2.eaa2e7f7.png"
+  },
+  {
+    "revision": "41d3007983f898267cbe880a5a031175",
+    "url": "/batp/static/media/gq3.41d30079.png"
+  },
+  {
+    "revision": "9578a4b37107ff1e25aedea809e2f2a3",
+    "url": "/batp/static/media/gq4.9578a4b3.png"
+  },
+  {
+    "revision": "14ae20b3a25be9a0fb3979dcac1b588f",
+    "url": "/batp/static/media/gq5.14ae20b3.png"
+  },
+  {
+    "revision": "2491e22e67f89ede8f2060b202344382",
+    "url": "/batp/static/media/iclass.2491e22e.png"
+  },
+  {
+    "revision": "3f195afdef6cbd2b45c56bcad8d77f01",
+    "url": "/batp/static/media/iclass1.3f195afd.png"
+  },
+  {
+    "revision": "e453a90ab0788280133889c9f389777a",
+    "url": "/batp/static/media/iclass2.e453a90a.png"
+  },
+  {
+    "revision": "a3dd613da8bea87c403d9f499a593463",
+    "url": "/batp/static/media/iclass3.a3dd613d.png"
+  },
+  {
+    "revision": "494fbd5a513864d9ea35373621daaac6",
+    "url": "/batp/static/media/iclass4.494fbd5a.png"
+  },
+  {
+    "revision": "809d2ccf169cf524833100927b7ff1c3",
+    "url": "/batp/static/media/iclass5.809d2ccf.png"
+  },
+  {
+    "revision": "04690f5732e2851dbf56dee23c1f8a73",
+    "url": "/batp/static/media/iclass6.04690f57.png"
+  },
+  {
+    "revision": "1e458d09efd60f7745e550c3de2b3379",
+    "url": "/batp/static/media/iclass7.1e458d09.png"
+  },
+  {
+    "revision": "392f6877c77faf7ca4c98c9b2c6ef2d2",
+    "url": "/batp/static/media/iclass8.392f6877.png"
+  },
+  {
+    "revision": "c9ff33ae87f3b9cc8e6ac9f14d5ab809",
+    "url": "/batp/static/media/iclass9.c9ff33ae.png"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/batp/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/batp/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/batp/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/batp/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/batp/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/batp/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "56bbfd20ec56a01b5fbfa877648f6771",
+    "url": "/batp/static/media/ovbs1.56bbfd20.png"
+  },
+  {
+    "revision": "1e2a5210f08fee99b36f587d6fcf28e5",
+    "url": "/batp/static/media/ovbs2.1e2a5210.png"
+  },
+  {
+    "revision": "9258b8ea56502e9f039c1d823d7c44c1",
+    "url": "/batp/static/media/ovbs3.9258b8ea.png"
+  },
+  {
+    "revision": "7da5dfa437c532b163e748852ec987d9",
+    "url": "/batp/static/media/ovbs4.7da5dfa4.png"
+  }
+]);
